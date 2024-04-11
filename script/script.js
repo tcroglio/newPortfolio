3e079baa17d420fa8812c0315d5e0f3def072862
tcroglio <tiago.roglio2112@gmail.com>
2024-04-10 22:27:57 -0300

    tab panel funcionando

---
+++ script/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tabLinks = document.querySelectorAll(".tab-link");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Evitar o comportamento padrão de redirecionamento do link
+
+            const tabId = this.getAttribute("data-tab");
+
+            tabContents.forEach(content => {
+                content.style.display = "none";
+            });
+
+            document.getElementById(tabId).style.display = "block";
+
+            tabLinks.forEach(link => {
+                link.classList.remove("active");
+            });
+
+            this.classList.add("active");
+        });
+    });
+
+    tabLinks[0].click();
+});
