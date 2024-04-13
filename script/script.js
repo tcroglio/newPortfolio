98f3b66c401e7908ef80224201b39287126ee31c
tcrogliO <tiago.roglio2112@gmail.com>
2024-04-13 20:08:03 -0300

    ajustando as funções do tab panel

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const tabLinks = document.querySelectorAll(".tab-link");
     const tabContents = document.querySelectorAll(".tab-content");
 
+    // Função para rolar a página para o topo
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+        tabLinks[0].click();
+    }
+
+
+    // Adiciona um ouvinte de evento ao carregar a página
+    window.addEventListener("load", scrollToTop);
+
     tabLinks.forEach(link => {
-        link.addEventListener("click", function(event) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Evitar o comportamento padrão de redirecionamento do link
 
             const tabId = this.getAttribute("data-tab");
 
